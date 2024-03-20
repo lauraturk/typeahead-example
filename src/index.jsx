1d978b2962f4844392f894445fd4e23505f0540b
lauraturk <laura.lk.turk@gmail.com>
2024-03-20 10:58:34 -0600

    update root render method

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 
 /**
-* Root of react site 
-*
-* Imports Helmet provider for the page head
-* And App which defines the content and navigation
-*/
+ * Root of react site
+ *
+ * Imports Helmet provider for the page head
+ * And App which defines the content and navigation
+ */
 
-// Render the site https://reactjs.org/docs/react-dom.html#render
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
