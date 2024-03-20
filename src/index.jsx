eb3c60281073129df2fe2d38318fb96ca3d072dc
lauraturk <laura.lk.turk@gmail.com>
2024-03-19 22:28:37 -0600

    bring back helmet for glitch?

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.jsx";
+import { HelmetProvider } from 'react-helmet-async';
 
 /**
- * Root of react site
- *
- * Imports Helmet provider for the page head
- * And App which defines the content and navigation
- */
+* Root of react site 
+*
+* Imports Helmet provider for the page head
+* And App which defines the content and navigation
+*/
 
 // Render the site https://reactjs.org/docs/react-dom.html#render
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
